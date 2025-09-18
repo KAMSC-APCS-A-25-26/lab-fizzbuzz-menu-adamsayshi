d5d91cc6a27ef0d01fbc961f0504f288e398d416
Adam <141070325+adamsayshi@users.noreply.github.com>
2025-09-18 13:20:25 -0400

    Update FizzBuzzMenu.java

--- src/main/java/FizzBuzzMenu.java
+++ src/main/java/FizzBuzzMenu.java
@@ -10,25 +10,37 @@ public class FizzBuzzMenu {
             System.out.println("1. Fizz\n2. Buzz\n3. FizzBuzz\n4. Exit");
             System.out.println("Enter your Choice:  ");
             int user = sc.nextInt();
-            for (int i = 1; i <= 100; i++) {
-                if (user == 1) {
-                    if (i%3 == 0){
-                        System.out.print(i + " ");
-                    }
-                } else if (user == 2) {
-                    if (i%5 == 0){
-                        System.out.print(i + " ");
-                    }
-                } else if (user == 3) {
-                    if (i%3 == 0 && i%5 == 0){
-                        System.out.print(i + " ");
-                    }
+
+                switch(user) {
+                        case 1:
+                            for (int i = 1; i <= 100; i++) {
+                                if (i % 3 == 0) {
+                                    System.out.print(i + " ");
+                                }
+                            }
+                            break;
+                        case 2:
+                            for (int i = 1; i <= 100; i++) {
+                                if (i % 5 == 0) {
+                                    System.out.print(i + " ");
+                                }
+                            }
+                            break;
+                        case 3:
+                            for (int i = 1; i <= 100; i++) {
+                                if (i % 3 == 0 && i % 5 == 0) {
+                                    System.out.print(i + " ");
+                                }
+                            }
+                            break;
+                        case 4:
+                            Running = false;
+                            System.out.println("Bye.");
+                            break;
+                        default:
+                            System.out.println("what");
+                            break;
                 }
-            }
-            if (user == 4) {
-                Running = false;
-                System.out.println("Bye.");
             }
         }
     }
-}
